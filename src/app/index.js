3155bcbf84764984dded58017250af03c25b3ad8
Sean Pollock <seanpollock3344@gmail.com>
2016-11-16 15:54:22 -0600

    making react bootstrap grid layout for two containers

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {Grid, Row, Col} from 'react-bootstrap'
 
 import ContactBox from './components/ContactBox'
 import WalletBox from './components/WalletBox'
@@ -8,9 +9,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <p> hello world form index.js</p>
-        <ContactBox />
-        <WalletBox />
+        <Grid>
+          <Col xs={12} md={6}>
+            <ContactBox />
+          </Col>
+          <Col xs={12} md={6}>
+            <WalletBox />
+          </Col>
+        </Grid>
       </div>
     )
   }
