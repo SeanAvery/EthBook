3ca536ed8bf74797212fee3b0884fa389e629ddb
Sean Pollock <seanpollock3344@gmail.com>
2016-11-16 15:46:53 -0600

    making entry point a react component and rendering it in the dom id app

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+import React from 'react'
+import {render} from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> hello world form index.js</p>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('app'))
